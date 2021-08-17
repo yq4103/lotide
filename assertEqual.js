90e69f6ca511e15f7000ddbcaf0c8c7aa44b6ff1
Yirong Qin <yq4103@mun.ca>
2021-08-17 01:05:21 +0000

    Implement assertEqual lotide

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return ('Assertion Passed: ' + [actual] + ' === ' + [expected]);
+  } else if (actual !== expected) {
+    return ('Assertion Failed: ' + [actual] + ' !== ' + [expected]);
+  }
+};
+
+// TEST CODE
+console.log(assertEqual("Bootcamp", "Bootcamp"));
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1, 2));
