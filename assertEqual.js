b6d7e17b5b8eb24e1ec59fcd9cbff3aaad66e256
Yirong Qin <yq4103@mun.ca>
2021-08-23 22:47:53 +0000

    refactored assertEqual and head with module.export and require added

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,16 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return ('Assertion Passed: ' + [actual] + ' === ' + [expected]);
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    return ('Assertion Failed: ' + [actual] + ' !== ' + [expected]);
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
+module.exports = assertEqual;
+
 // TEST CODE
-console.log(assertEqual("Bootcamp", "Bootcamp"));
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
-console.log(assertEqual(1, 2));
+//assertEqual("Bootcamp", "Bootcamp");
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+//assertEqual(1, 2);
