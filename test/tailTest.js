48c5f0949b773594ef9b24a8a3e0dad00d214145
Yirong Qin <yq4103@mun.ca>
2021-08-24 01:54:56 +0000

    modifed tailTest so that mocha and chai are useable

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,12 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
+//const tail = require('../tail');
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns 3 for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), 3);
+  });
+
+});
