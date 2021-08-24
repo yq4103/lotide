3fdef0f5a7cb2353d8bc2d5c3c02bb33d6a3c06a
Yirong Qin <yq4103@mun.ca>
2021-08-24 02:26:07 +0000

    use deepEqual to compare the arrays according to Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,15 @@
 const middle = require ('../middle');
-const assertArraysEqual = require ('../assertArraysEqual');
+//const assertArraysEqual = require ('../assertArraysEqual');
+const assert = require('chai').assert;
 
-middle([3]);
-middle([1, 2, 3, 4, 5]);
-middle([1, 2, 3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+});
+
+//middle([3]);
+//middle([1, 2, 3, 4, 5]);
+//middle([1, 2, 3, 4]);
+//assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
