6a1ff9cac7b2114cd9eaccbb61137524e3b38344
Yirong Qin <yq4103@mun.ca>
2021-08-24 02:57:35 +0000

    incomplete index

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const assertArraysEqual = require('./assertArraysEqual')
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual
+  head,
+  tail,
+  middle,
+  assertEqual,
+  eqArrays,
+  assertArraysEqual
 };
